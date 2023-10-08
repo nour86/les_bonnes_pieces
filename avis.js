36b5c71aa389ffccdbf9e9eab4b348a21fe3c4c2
Nour Alchami <n.alchami@gmail.com>
2023-10-08 14:25:55 +0200

    exercice 3

---
+++ avis.js
@@ -0,0 +1,9 @@
+export function ajoutListenersAvis() {
+    const piecesElements = document.querySelectorAll(".fiches article button");
+
+    for (let i = 0; i < piecesElements.length; i++) {
+      piecesElements[i].addEventListener("click", async function (event) {
+           /* ... */
+      });
+    }
+}
